4f3f2c3e24833a7a8a4a9cf5c2f94f554637e11d
Alan Autrand <alan.autrand@nextiva.com>
2019-04-07 21:40:46 -0700

    First HackerRank practice problem. 30 Day of code. Data Types.

---
+++ hackerrank/30daysofcode/day1_data_types.py
@@ -0,0 +1,25 @@
+import sys
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+
+import sys
+# Read and save an integer, double, and String to your variables. 
+data = sys.stdin.readlines()
+a = data[0]
+b = data[1]
+c = data[2]
+
+# Print the sum of both integer variables on a new line.
+print(i+int(a))
+
+# Print the sum of the double variables on a new line.
+print(d+float(b))
+
+# Concatenate and print the String variables on a new line
+print(s+c)
+
+# The 's' variable above should be printed first.
+
