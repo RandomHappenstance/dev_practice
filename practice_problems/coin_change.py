5c235ef56eb114aaa03f825aa6aba1da4a329f15
Alan Autrand <alan.autrand@gmail.com>
2019-07-29 08:41:35 -0700

    Adding coin change solution.

--- practice_problems/coin_change.py
+++ practice_problems/coin_change.py
@@ -10,5 +10,33 @@ The problem is described as:
 
 # Lets say that I have to return 33 cents.
 
-def return_change(amount_to_return):
-    pass
+def return_change(amount):
+    coin_types = (25, 10, 5, 1)
+    
+    change = {"25" : 0,
+              "10" : 0,
+              "5" : 0,
+              "1" : 0 }
+
+    while amount != 0:
+        if amount > 25:
+            change['25'] += 1
+            amount -= 25
+        elif amount > 10:
+            change['10'] += 1
+            amount -= 10
+        elif amount > 5:
+            change['5'] += 1
+            amount -= 5
+        elif amount >= 1:
+            change['1'] += 1       
+            amount -= 1
+    return change
+
+if __name__ == "__main__":
+    change_amount = 33 # 33 cents
+    change = return_change(change_amount)
+    print("Amount to return is: ")
+    print(change)
+    print(change_amount)
+
