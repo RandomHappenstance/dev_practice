15dcf3eb816875a033ea202374f9f1acc8363369
Alan Autrand <alan.autrand@gmail.com>
2020-05-10 15:27:56 -0700

    Adding tests for Nodes

--- core_cs/binary_trees/test_binary_tree.py
+++ core_cs/binary_trees/test_binary_tree.py
@@ -1,12 +1,20 @@
 import unittest
-from binary_tree import BinaryTree as BT
+from binary_tree import BinaryTree
+from binary_tree import Node
+
+
+class NodeTests(unittest.TestCase):
+
+    def test_node_instantiation(self):
+        node = Node()
+        assert isinstance(node, Node)
 
 
 class BinaryTreeTests(unittest.TestCase):
 
     def test_bt_instantiation(self):
-        bt = BT()
-        assert isinstance(bt, BT)
+        bt = BinaryTree()
+        assert isinstance(bt, BinaryTree)
 
 
 if __name__ == '__main__':
