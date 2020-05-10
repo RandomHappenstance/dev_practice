2254d81d5d859b1cff2b90923f9654a978daf7b6
Alan Autrand <alan.autrand@gmail.com>
2020-05-10 16:18:15 -0700

    Added implementation for binary tree insert

--- core_cs/binary_trees/binary_tree.py
+++ core_cs/binary_trees/binary_tree.py
@@ -6,4 +6,27 @@ class Node:
         self.right = None
 
 class BinaryTree:
-    pass
+    """ This Binary Tree is made under the assumption that we are only handling integers with it. """
+    def __init__(self):
+        self.root = None
+
+    def get_root(self):
+        return self.root
+
+    def add(self, value):
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            self._add(self.root, value)
+
+    def _add(self, node, value):
+        if value < node.value:
+            if node.left is not None:
+                self._add(node.left, value)
+            else:
+                node.left = Node(value)
+        else :
+            if node.right is not None:
+                self._add(node.right, value)
+            else:
+                node.right = Node(value)
--- core_cs/binary_trees/test_binary_tree.py
+++ core_cs/binary_trees/test_binary_tree.py
@@ -7,9 +7,32 @@ from binary_tree import Node
 
 class BinaryTreeTests(unittest.TestCase):
 
-    def test_bt_instantiation(self):
+    def test_instantiation(self):
         bt = BinaryTree()
         assert isinstance(bt, BinaryTree)
+
+    def test_get_root(self):
+        bt = BinaryTree()
+        assert bt.get_root() == None
+
+    def test_add_node(self):
+        bt = BinaryTree()
+        bt.add(5)
+        assert bt.get_root().value == 5
+
+    def test_insert_lower(self):
+        bt = BinaryTree()
+        bt.add(5)
+        bt.add(3)
+        assert bt.get_root().left.value == 3
+        assert bt.get_root().right is None
+
+    def test_insert_higher(self):
+        bt = BinaryTree()
+        bt.add(5)
+        bt.add(8)
+        assert bt.get_root().right.value == 8
+        assert bt.get_root().left is None
 
 
 class NodeTests(unittest.TestCase):
