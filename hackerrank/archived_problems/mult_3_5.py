645f0e87441f9c7ab723b594ccb301965bc0fdfc
Alan Autrand <alan.autrand@gmail.com>
2021-02-20 22:10:54 -0700

    Changing the structure of the proj euler solutions file. Making every problem a function outside of a class. Also adding first problem functionality and tests.

--- hackerrank/archived_problems/mult_3_5.py
+++ hackerrank/archived_problems/mult_3_5.py
@@ -1,18 +1,29 @@
 """
-If we list all the natural numbers below 10 that are multiples of 3 or 5,
-we get 3, 5, 6 and 9. The sum of these multiples is 23.
-
-Find the sum of all the multiples of 3 or 5 below 1000.
+    All The Project Euler archive problems.
 """
 
 
-class Euler:
-    """ A class that holds all problems from Project Euler. """
+def mult_sum(value: int) -> int:
+    """ Problem 1.
 
-    def mult_sum(self, value: int = 0) -> int:
-        """ Problem 1.
+    If we list all the natural numbers below 10 that are multiples of 3 or 5,
+    we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
-        Returns:
-            object:
-        """
-        return 0
+    Find the sum of all the multiples of 3 or 5 below 1000.
+
+    Returns:
+        object:
+    """
+
+    def three_or_five(val) -> bool:
+        if val % 3 == 0 or val % 5 == 0:
+            return True
+        else:
+            return False
+
+    total = 0
+    for num in range(0, value):
+        if three_or_five(num):
+            total += num
+
+    return total
--- tests/test_euler.py
+++ tests/test_euler.py
@@ -1,11 +1,16 @@
 import unittest
 
-from hackerrank.archived_problems.mult_3_5 import Euler
+from hackerrank.archived_problems.mult_3_5 import mult_sum
 
 
-class MyTestCase(unittest.TestCase):
+class EulerTestCases(unittest.TestCase):
     def test_prob_1(self):
         """ """
-        sum = Euler.mult_sum(10)
+
+        sum = mult_sum(10)
         print(sum)
         self.assertEqual(sum, 23)
+
+        sum = mult_sum(1000)
+        print(sum)
+        self.assertEqual(sum, 233168)
