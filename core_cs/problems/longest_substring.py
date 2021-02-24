2e3c5a8aacb1391dcee28a8d27cbd3122cdcc828
Alan Autrand <alan.autrand@gmail.com>
2021-02-23 22:10:12 -0700

    Adding template for rich dashboard.

---
+++ core_cs/problems/longest_substring.py
@@ -0,0 +1,7 @@
+"""
+    src: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+    started: 01/23/21
+    desc: Given a string s, find the length of the longest substring without repeating characters.
+"""
+
+
---
+++ frameworks/rich/astronomy_dashboard.py
@@ -0,0 +1,9 @@
+# Use rich to make a portal for the perseverance rover.
+# include sol
+# list and count of pics for the day.
+
+
+from rich.console import Console
+
+console = Console()
+console.print("Hello", "World!", style="bold red")
