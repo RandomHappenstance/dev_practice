2f27021df8b5c30c7688b0feea88d5372256192a
Alan Autrand <alan.autrand@gmail.com>
2021-02-25 18:23:14 -0700

    Adding rich and tkinter frameworks

--- frameworks/rich/astronomy_dashboard.py
+++ frameworks/rich/astronomy_dashboard.py
@@ -7,3 +7,9 @@ from rich.console import Console
 
 console = Console()
 console.print("Hello", "World!", style="bold red")
+
+
+# This library is pretty cool. Lots of potential. But its not was I was looking for.
+# Its used more to display text with more bells and whistles. I was hoping for a framework to
+# create a UI in the console. Be able to make side columns, bounding boxes, live updates.
+#
