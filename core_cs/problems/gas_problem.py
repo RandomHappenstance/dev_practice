d14b6f4abc9082cae1dc537cefb1388b663b9a9c
Alan Autrand <alan.autrand@gmail.com>
2021-03-17 21:54:47 -0700

    Adding gas problem function.

---
+++ core_cs/problems/gas_problem.py
@@ -0,0 +1,8 @@
+# if I have two gas stations 5 miles apart, if I knew the prices,
+# should I go to the one 5miles away or stay with this one.
+DISTANCE_TO_STATION = 5
+
+
+def should_i_go(mpg: int, cost_at_a: float, cost_at_b: float) -> bool:
+
+    return True
